1261b6c6f9bd06b2230704d0f494a7854da05bb1
klauza <klauza.uk@gmail.com>
2019-06-27 19:27:05 +0100

    portfolio page - content has been added

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import image from '../../media/about-pc.jpeg';
-import spinner from './loader/load.gif';
+// import spinner from './loader/load.gif';
 
 
 
@@ -56,7 +56,7 @@ export default class About extends Component {
       <p className="about__top">Hey there, my name is Michal and I'm glad you came here to see my portfolio. Alright, without further delay I'd like you to read few words about me.</p>	
       <h1 className="about__mid"><span id="about-text">ABOUT</span></h1>
       <h1 className="about__mid-right"><span id="me-text">ME</span></h1>
-      <p className="about__bottom">Web development was around me for quite a while, but in serious way I started to think about it since 2018. I enjoy coding, struggling with problems, creating something from nothing. The final results provide me phenomenal feelings and injection of positivity. For last period of time I was practicing with React. </p>
+      <p className="about__bottom">Web development was around me for quite a while, but in serious way I started to think about it since 2018. I enjoy coding, struggling with problems, creating something from nothing. The final results provide me phenomenal feelings and injection of positivity. Mainly I'm creating SPA applications. For last period of time I was practicing with React. </p>
       <figure><img src={image} alt="" className="about__image" onLoad={this.handleImageLoaded.bind(this)} onError={this.handleImageErrored.bind(this)} /></figure>
      
     </section>
--- src/components/layout/Portfolio.js
+++ src/components/layout/Portfolio.js
@@ -6,46 +6,59 @@ function Portfolio() {
     <div id="portfolio-page">
       {/* <div className="shadow"></div> */}
       <div className="portfolio-grid">
-        <div className="card">
 
+        <div className="card">
           <div className="card-background"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
           <div className="top-touch"></div>
           <div className="card-description">
-            <div className="card-description__title">Photography</div>
-            <div className="card-description__language">React HTML CSS</div>
-            <div className="card-description__icon"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
-            <div className="card-description__link"><a href="#">see page</a><a href="#">see github</a></div>
+            <div className="card-description__title">coffer</div>
+            <div className="card-description__language">JS state pattern, History API</div>
+            <div className="card-description__icon"><i className="fa fa-camera-retro"></i></div>
+            <div className="card-description__link"><a href="https://google.com">see page</a><a href="https://google.com">see code<i className="fa fa-github"></i></a></div>
           </div>
-          
-        </div>
 
-
-
-
-
-        <div className="card">
-          <div className="image"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
-          <div className="title">Photography</div>
-          <div className="language">React</div>
+          <div className="card-description__hover--text">This project is a container of any smaller idea. Was made to study JS state.</div>
         </div>
 
         <div className="card">
-          <div className="image"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
-          <div className="title">Photography</div>
-          <div className="language">React</div>
+          <div className="card-background"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
+          <div className="top-touch"></div>
+          <div className="card-description">
+            <div className="card-description__title">taskMission - game</div>
+            <div className="card-description__language">JS revealing pattern, fetch API, localStore</div>
+            <div className="card-description__icon"><i className="fa fa-camera-retro"></i></div>
+            <div className="card-description__link"><a href="https://google.com">see page</a><a href="https://google.com">see code</a></div>
+          </div>
+
+          <div className="card-description__hover--text">Actor game where you choose the popular celebrity to win... something.</div>
         </div>
 
         <div className="card">
-          <div className="image"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
-          <div className="title">Photography</div>
-          <div className="language">React</div>
+          <div className="card-background"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
+          <div className="top-touch"></div>
+          <div className="card-description">
+            <div className="card-description__title">guessNumber</div>
+            <div className="card-description__language">JS Classes, local Storage, Regex</div>
+            <div className="card-description__icon"><i className="fa fa-camera-retro"></i></div>
+            <div className="card-description__link"><a href="https://google.com">see page</a><a href="https://google.com">see code</a></div>
+          </div>
+
+          <div className="card-description__hover--text">Simply guess a number game. I believe there is no need to explain what to do.</div>
         </div>
-        
+
         <div className="card">
-          <div className="image"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
-          <div className="title">Photography</div>
-          <div className="language">React</div>
+          <div className="card-background"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
+          <div className="top-touch"></div>
+          <div className="card-description">
+            <div className="card-description__title">heroproject</div>
+            <div className="card-description__language">React, Sass</div>
+            <div className="card-description__icon"><i className="fa fa-sellsy"></i></div>
+            <div className="card-description__link"><a href="https://google.com">see page</a><a href="https://google.com">see code</a></div>
+          </div>
+
+          <div className="card-description__hover--text">Very first steps to React, there is firebase used in background. The user can login to create his own 2d hero.</div>
         </div>
+
 
       </div>
 
@@ -53,20 +66,51 @@ function Portfolio() {
         <p className="separator-para">Some very first projects...</p>
       </div>
 
-
       <div className="portfolio-grid">
+
+
         <div className="card">
-
-          <div className="image"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
-          <div className="bot-part">
-            <div className="title">Photography</div>
-            <div className="language">React</div>
-            <div className="link"><a href="#">LINK</a></div>
+          <div className="card-background"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
+          <div className="top-touch"></div>
+          <div className="card-description">
+            <div className="card-description__title">photography</div>
+            <div className="card-description__language">HTML, Sass, jQuery</div>
+            <div className="card-description__icon"><i className="fa fa-camera-retro"></i></div>
+            <div className="card-description__link"><a href="https://google.com">see page</a><a href="https://google.com">see code</a></div>
           </div>
-          
-        </div>
-      </div>
 
+          <div className="card-description__hover--text">Hobbystic photography SPA with some jQuery DOM manipulation.</div>
+        </div>
+
+
+        <div className="card">
+          <div className="card-background"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
+          <div className="top-touch"></div>
+          <div className="card-description">
+            <div className="card-description__title">shelter</div>
+            <div className="card-description__language">HTML, Sass</div>
+            <div className="card-description__icon"><i className="fa fa-comment-o"></i></div>
+            <div className="card-description__link"><a href="https://google.com">see page</a><a href="https://google.com">see code</a></div>
+          </div>
+
+          <div className="card-description__hover--text">Sass based One page website. Made with great CSS GRID tool.</div>
+        </div>
+
+
+        <div className="card">
+          <div className="card-background"><img src="https://www.w3schools.com/w3css/img_lights.jpg" alt=""/></div>
+          <div className="top-touch"></div>
+          <div className="card-description">
+            <div className="card-description__title">hussars</div>
+            <div className="card-description__language">HTML, Sass, jQuery</div>
+            <div className="card-description__icon"><i className="fa fa-pied-piper-alt"></i></div>
+            <div className="card-description__link"><a href="https://google.com">see page</a><a href="https://google.com">see code</a></div>
+          </div>
+
+          <div className="card-description__hover--text">Horse cavalry at the turn of XVI century. One page website with colorful sections.</div>
+        </div>
+
+      </div>
 
     </div>
   )
