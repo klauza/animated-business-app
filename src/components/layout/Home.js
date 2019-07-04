28c9d0f2f44c515e3f78584e127c35be391c231f
klauza <klauza.uk@gmail.com>
2019-07-04 11:50:04 +0100

    home content add

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import heroImg from '../../media/hero-imgs/hero.jpg'
 
 function Home() {
   return (
     <div className="container-home">
-      <h1>home</h1>
+
+      
+
+      <div className="container-home__hero">
+        <img src={heroImg} className="container-home__hero--img" />
+        
+        <div className="container-home__hero--para">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam, vel?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quae vero excepturi porro odit minus officiis ex fugiat enim pariatur!</p>
+        </div>
+
+      </div>
+
+      <div className="container-home__experience">
+        <span>EXPERIENCE</span>
+      </div>
+
+      <div className="container-home__skill-icons">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
     </div>
   )
 }
