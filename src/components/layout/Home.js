fb83b760f74738bd0d90b92a4be1114ab66b5a59
klauza <klauza.uk@gmail.com>
2019-07-04 12:40:44 +0100

    change to class component, spinner add

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,36 +1,64 @@
-import React from 'react'
-import heroImg from '../../media/hero-imgs/hero.jpg'
+import React, { Component } from 'react';
+import heroImg from '../../media/hero-imgs/hero.jpg';
+import spinner from '../../media/loader2.gif';
 
-function Home() {
-  return (
-    <div className="container-home">
+export default class Home extends Component {
+  componentDidMount(){
+    document.querySelector('.container-home').style.opacity ="0";
+    document.querySelector('.home-spinner').style.opacity = "1";
+  }
+
+  constructor(props){
+    super(props);
+    this.state = { loading: true}
+  }
+  handleImageLoaded(){ 
+    this.setState({ loading: false});
+    this.startHome();
+  }
+  handleImageErrored() {
+    this.setState({ loading: true});
+  }
+  startHome(){
+    document.querySelector('.container-home').style.opacity = "1";
+    document.querySelector('.home-spinner').style.opacity = "0";
+    document.querySelector('.home-spinner').style.visibility = "none";
+  }
+
+  render(){
+
+    return (
+      <div className="home">
+        <div className="home-spinner">
+          <img src={spinner} alt=""/>
+        </div>
+        <div className="container-home">
 
       
+          <div className="container-home__hero">
+            <img src={heroImg} className="container-home__hero--img" onLoad={this.handleImageLoaded.bind(this)} onError={this.handleImageErrored.bind(this)} />
+            
+            <div className="container-home__hero--para">
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam, vel?</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quae vero excepturi porro odit minus officiis ex fugiat enim pariatur!</p>
+            </div>
 
-      <div className="container-home__hero">
-        <img src={heroImg} className="container-home__hero--img" />
-        
-        <div className="container-home__hero--para">
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam, vel?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quae vero excepturi porro odit minus officiis ex fugiat enim pariatur!</p>
+          </div>
+
+          <div className="container-home__experience">
+            <span>EXPERIENCE</span>
+          </div>
+
+          <div className="container-home__skill-icons">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+          </div>
         </div>
-
       </div>
-
-      <div className="container-home__experience">
-        <span>EXPERIENCE</span>
-      </div>
-
-      <div className="container-home__skill-icons">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
-
-export default Home
