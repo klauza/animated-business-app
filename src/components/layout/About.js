f69f9c22dd116bdd91c2f7113f595bce84dcfe98
klauza <klauza.uk@gmail.com>
2019-07-06 01:29:40 +0100

    added some css

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -62,10 +62,10 @@ export default class About extends Component {
       </div>
 
       <div className="about">
-        <p className="about__top">Hey there, my name is Michal and I'm glad you came here to see my portfolio. Alright, without further delay I'd like you to read few words about me.</p>	
+        <div className="about__top">Hey there, my name is Michal and <div className="secret-text">I'm glad</div> you came here to see my portfolio. <div className="secret-text-two">Alright</div>, without further delay I'd like you to read few words about me.</div>	
         <h1 className="about__mid"><span id="about-text">ABOUT</span></h1>
         <h1 className="about__mid-right"><span id="me-text">ME</span></h1>
-        <p className="about__bottom">Web development was around me for quite a while, but in serious way I started to think about it since 2018. I enjoy coding, struggling with problems, creating something from nothing. The final results provide me phenomenal feelings and injection of positivity. Mainly I'm creating Single Page Applications. For last period of time I was practicing with React. </p>
+        <p className="about__bottom">Web development was around me for quite a while, but in serious way I started to think about it since 2018. I enjoy coding, struggling with problems, creating something from nothing. The final results provide me phenomenal feelings and injection of positivity. Mainly I'm creating Single Page Applications. For last period of time I was practicingwith React.</p>
         <figure><img src={image} alt="" className="about__image" onLoad={this.handleImageLoaded.bind(this)} onError={this.handleImageErrored.bind(this)} /></figure>
       </div>
     </div>
