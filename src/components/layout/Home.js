e401f51472cdaaaa08c364c9b01b895d93c98b43
klauza <klauza.uk@gmail.com>
2019-07-25 00:23:13 +0100

    separate skills out of home component

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import heroImg from '../../media/hero-imgs/hero.jpg';
 import spinner from '../../media/loader2.gif';
-import webpackImg from '../../media/hero-imgs/webpack.png';
 import animateBlocks from './shadow.js';
+import HomeSkills from './HomeSkills';
 
 export default class Home extends Component {
   componentDidMount(){
@@ -85,17 +85,8 @@ export default class Home extends Component {
             <span>EXPERIENCE</span>
           </div>
 
-          <div className="container-home__skill-icons">
-            <div className="skill-block-parent"><div data-delay="1" className="icon-container"><i className="fab fa-js-square"></i><div className="desc">JavaScript</div></div></div>
-            <div className="skill-block-parent"><div data-delay="2" className="icon-container"><i className="fab fa-react"></i><div className="desc">React</div></div></div>
-            <div className="skill-block-parent"><div data-delay="3" className="icon-container"><i className="fab fa-sass"></i><div className="desc">Sass</div></div></div>
-            <div className="skill-block-parent"><div data-delay="4" className="icon-container"><i className="fas fa-tools"></i><div className="desc">Regex</div></div></div>
-            <div className="skill-block-parent"><div data-delay="5" className="icon-container"><i className="fas fa-code-branch"></i><div className="desc">Github</div></div></div>
-            <div className="skill-block-parent"><div data-delay="6" className="icon-container"><i className="fab fa-html5"></i><div className="desc">HTML</div></div></div>
-            <div className="skill-block-parent"><div data-delay="7" className="icon-container"><i className="fab fa-bootstrap"></i><div className="desc">Bootstrap</div></div></div>
-            <div className="skill-block-parent"><div data-delay="8" className="icon-container"><i className="fas fa-boxes"></i><div className="desc">RESTful API</div></div></div>
-            <div className="skill-block-parent"><div data-delay="9" className="icon-container"><img src={webpackImg} alt=""/><div className="desc">Bundlers</div></div></div>
-          </div>
+          <HomeSkills />
+          
         </div>
       </div>
     )
