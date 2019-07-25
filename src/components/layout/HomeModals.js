fbf226f5c0619fd2430109203a2c10b329d21db6
klauza <klauza.uk@gmail.com>
2019-07-25 01:08:19 +0100

    add modals for experience blocks

--- src/components/layout/HomeModals.js
+++ src/components/layout/HomeModals.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import M from 'materialize-css/dist/js/materialize.min.js';
+import React, {Fragment} from 'react';
 
 const HomeModals = () => {
   return (
-    <div id="javascript-modal" className="modal" style={modalStyle}>
-      <h2>this is js modal</h2>
-    </div>
+    <Fragment>
+      <div id="javascript-modal" className="modal" style={modalStyle}>
+        <h2>this is js modal</h2>
+      </div>
+      <div id="react-modal" className="modal" style={modalStyle}>
+        <h2>this is react modal</h2>
+      </div>
+      <div id="sass-modal" className="modal" style={modalStyle}>
+        <h2>this is sass modal</h2>
+      </div>
+      <div id="regex-modal" className="modal" style={modalStyle}>
+        <h2>this is regex modal</h2>
+      </div>
+      <div id="github-modal" className="modal" style={modalStyle}>
+        <h2>this is github modal</h2>
+      </div>
+      <div id="html-modal" className="modal" style={modalStyle}>
+        <h2>this is html modal</h2>
+      </div>
+      <div id="bootstrap-modal" className="modal" style={modalStyle}>
+        <h2>this is bootstrap modal</h2>
+      </div>
+      <div id="rest-modal" className="modal" style={modalStyle}>
+        <h2>this is restful api modal</h2>
+      </div>
+      <div id="bundlers-modal" className="modal" style={modalStyle}>
+        <h2>this is bundlers modal</h2>
+      </div>
+    </Fragment>
   )
 }
 
 const modalStyle = {
   width: '75%',
   height: '75%'
-};
+};  
+
+
 
 export default HomeModals
--- src/components/layout/HomeSkills.js
+++ src/components/layout/HomeSkills.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import webpackImg from '../../media/hero-imgs/webpack.png';
-import M from 'materialize-css/dist/js/materialize.min.js';
 
 const HomeSkills = () => {
   return (
     <div className="container-home__skill-icons">
-      <a href="#javascript-modal" className="modal-trigger skill-block-parent"><div data-delay="1" className="icon-container"><i className="fab fa-js-square"></i><div className="desc">JavaScript</div></div></a>
-      <a href="#" className="skill-block-parent"><div data-delay="2" className="icon-container"><i className="fab fa-react"></i><div className="desc">React</div></div></a>
-      <a href="#" className="skill-block-parent"><div data-delay="3" className="icon-container"><i className="fab fa-sass"></i><div className="desc">Sass</div></div></a>
-      <a href="#" className="skill-block-parent"><div data-delay="4" className="icon-container"><i className="fas fa-tools"></i><div className="desc">Regex</div></div></a>
-      <a href="#" className="skill-block-parent"><div data-delay="5" className="icon-container"><i className="fas fa-code-branch"></i><div className="desc">Github</div></div></a>
-      <a href="#" className="skill-block-parent"><div data-delay="6" className="icon-container"><i className="fab fa-html5"></i><div className="desc">HTML</div></div></a>
-      <a href="#" className="skill-block-parent"><div data-delay="7" className="icon-container"><i className="fab fa-bootstrap"></i><div className="desc">Bootstrap</div></div></a>
-      <a href="#" className="skill-block-parent"><div data-delay="8" className="icon-container"><i className="fas fa-boxes"></i><div className="desc">RESTful API</div></div></a>
-      <a href="#" className="skill-block-parent"><div data-delay="9" className="icon-container"><img src={webpackImg} alt=""/><div className="desc">Bundlers</div></div></a>
+      <div href="#javascript-modal" className="modal-trigger skill-block-parent"><div data-delay="1" className="icon-container"><i className="fab fa-js-square"></i><div className="desc">JavaScript</div></div></div>
+      <div href="#react-modal" className="modal-trigger skill-block-parent"><div data-delay="2" className="icon-container"><i className="fab fa-react"></i><div className="desc">React</div></div></div>
+      <div href="#sass-modal" className="modal-trigger skill-block-parent"><div data-delay="3" className="icon-container"><i className="fab fa-sass"></i><div className="desc">Sass</div></div></div>
+      <div href="#regex-modal" className="modal-trigger skill-block-parent"><div data-delay="4" className="icon-container"><i className="fas fa-tools"></i><div className="desc">Regex</div></div></div>
+      <div href="#github-modal" className="modal-trigger skill-block-parent"><div data-delay="5" className="icon-container"><i className="fas fa-code-branch"></i><div className="desc">Github</div></div></div>
+      <div href="#html-modal" className="modal-trigger skill-block-parent"><div data-delay="6" className="icon-container"><i className="fab fa-html5"></i><div className="desc">HTML</div></div></div>
+      <div href="#bootstrap-modal" className="modal-trigger skill-block-parent"><div data-delay="7" className="icon-container"><i className="fab fa-bootstrap"></i><div className="desc">Bootstrap</div></div></div>
+      <div href="#rest-modal" className="modal-trigger skill-block-parent"><div data-delay="8" className="icon-container"><i className="fas fa-boxes"></i><div className="desc">RESTful API</div></div></div>
+      <div href="#bundlers-modal" className="modal-trigger skill-block-parent"><div data-delay="9" className="icon-container"><img src={webpackImg} alt=""/><div className="desc">Bundlers</div></div></div>
     </div>
   )
 }
