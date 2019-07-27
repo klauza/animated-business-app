a2f612511a81da4396bdf87a7fd9f52025e343f1
klauza <klauza.uk@gmail.com>
2019-07-28 00:44:27 +0100

    nav css fixes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,16 +7,16 @@ import navLogo from '../../media/icon.png';
 function Navbar() {
   return (
     <div className="nav-bar">
-      <div className="container">
-        <div className="nav-logo">
+      <div className="nav-container">
+        <div className="my-nav-logo">
           <img src={navLogo} alt=""/>
         </div>
 
         <div className="nav-links-container">
-          <NavLink exact={true} to="/" className="nav-link">Home</NavLink>
-          <NavLink to="/about" className="nav-link">About</NavLink>
-          <NavLink to="/portfolio" className="nav-link">Portfolio</NavLink>
-          <NavLink to="/github" className="nav-link">Github</NavLink>
+          <NavLink exact={true} to="/" className="nav-links">Home</NavLink>
+          <NavLink to="/about" className="nav-links">About</NavLink>
+          <NavLink to="/portfolio" className="nav-links">Portfolio</NavLink>
+          <NavLink to="/github" className="nav-links">Github</NavLink>
         </div>
       
         <div className="nav-icons">
