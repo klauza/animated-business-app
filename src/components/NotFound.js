db10dd15b657ec48f388ed580c083690db517a84
klauza <klauza.uk@gmail.com>
2019-11-30 14:39:12 +0000

    add css for nightMode & NotFound comps

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div>
+    <div className="not-found-page">
       <h1>page not found</h1>
       <p>and it does not exist</p>
     </div>
