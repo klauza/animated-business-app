ba7960711770a3dbed59b475d440e3e6c402ca42
Michal Klauza <klauza.uk@gmail.com>
2021-12-14 12:41:39 +0100

    Projects route renovation/edit

--- src/components/Projects/ProjectsData.js
+++ src/components/Projects/ProjectsData.js
@@ -1,54 +1,67 @@
-import { eNewsstandImg, bhutanotel, nobleImg, heroImg, hussarImg, petShelterImg, photographyImg, pwdMaker} from '../../media/index';
+import {
+  eNewsstandImg,
+  bhutanotel,
+  nobleImg,
+  heroImg,
+  hussarImg,
+  petShelterImg,
+  photographyImg,
+  pwdMaker,
+} from '../../media/index';
 // import { carDealershipImg, churchImg } from '../../media/index';
 
-const portfData =  [
+const portfData = [
   {
     id: 1,
-    cardClass: "e-newsstand-card",
+    cardClass: 'e-newsstand-card',
     image: eNewsstandImg,
-    title: "e-newsstand",
-    language: "React, Redux, node.js",
-    icons: ["fab fa-react", "fas fa-database"],
-    completed: "in dev",
-    live_page: "https://e-newsstand.herokuapp.com/",
-    github_page: "https://github.com/klauza/e-newsstand",
-    description: "E-commerce project with paying engine, state/url query search and filtering"
+    title: 'Golden Boots',
+    language: 'For longest travel made in one day',
+    icons: ['fas fa-trophy', 'fas fa-star'],
+    completed: 'Link 1',
+    live_page: 'https://e-newsstand.herokuapp.com/',
+    github_page: 'https://github.com/klauza/e-newsstand',
+    description:
+      'Phasellus et aliquet erat. Fusce ornare elit quis erat interdum, et pellentesque quam congue. Duis laoreet, libero id hendrerit lobortis, nunc quam commodo felis, a aliquam sem diam at metus.',
   },
   {
     id: 2,
-    cardClass: "bhutanotel-card",
+    cardClass: 'bhutanotel-card',
     image: bhutanotel,
-    title: "Bhutanotel",
-    language: "React, Redux, styled-components",
-    icons: ["fab fa-react"],
-    completed: "see page",
-    live_page: "https://bhutanotel.netlify.com/",
-    github_page: "https://github.com/klauza/bhutanotel",
-    description: "Hotel page concept with booking option"
+    title: 'Bhutanotel',
+    language: 'Beautiful hotel photo in Bhutan',
+    icons: ['fas fa-camera-retro'],
+    completed: 'Link 1',
+    live_page: 'https://bhutanotel.netlify.com/',
+    github_page: 'https://github.com/klauza/bhutanotel',
+    description:
+      'Phasellus et aliquet erat. Fusce ornare elit quis erat interdum, et pellentesque quam congue. Duis laoreet, libero id hendrerit lobortis, nunc quam commodo felis, a aliquam sem diam at metus.',
   },
   {
     id: 3,
-    cardClass: "noble-leaders-card",
+    cardClass: 'noble-leaders-card',
     image: nobleImg,
-    title: "Game hub",
-    language: "React, Redux, MongoDB",
-    icons: ["fab fa-react", "fas fa-database"],
-    completed: "see page",
-    live_page: "https://noble-leaders.herokuapp.com/",
-    github_page: "https://github.com/klauza/noble-leaders",
-    description: "A gaming platform where you gain points for playing"
+    title: 'Technology among us',
+    language: 'Award for senior photo-game-competition',
+    icons: ['fas fa-star', 'fas fa-camera-retro'],
+    completed: 'Link 1',
+    live_page: 'https://noble-leaders.herokuapp.com/',
+    github_page: 'https://github.com/klauza/noble-leaders',
+    description:
+      'Phasellus et aliquet erat. Fusce ornare elit quis erat interdum, et pellentesque quam congue. Duis laoreet, libero id hendrerit lobortis, nunc quam commodo felis, a aliquam sem diam at metus.',
   },
   {
     id: 4,
-    cardClass: "regex-card",
+    cardClass: 'regex-card',
     image: pwdMaker,
-    title: "password-maker",
-    language: "JS, regexp",
-    icons: ["fab fa-js-square"],
-    completed: "see page",
-    live_page: "https://pwd-maker-klauza.netlify.com/",
-    github_page: "https://github.com/klauza/password-maker",
-    description: "Generates regular expression password from the chosen settings"
+    title: 'security in 2044',
+    language: 'Award for "security in 2044"',
+    icons: ['fas fa-star', 'fas fa-camera-retro'],
+    completed: 'Link 1',
+    live_page: 'https://pwd-maker-klauza.netlify.com/',
+    github_page: 'https://github.com/klauza/password-maker',
+    description:
+      'Phasellus et aliquet erat. Fusce ornare elit quis erat interdum, et pellentesque quam congue. Duis laoreet, libero id hendrerit lobortis, nunc quam commodo felis, a aliquam sem diam at metus.',
   },
   // {
   //   id: 5,
@@ -88,54 +101,58 @@ const portfData =  [
   // },
   {
     id: 8,
-    cardClass: "hero-card",
+    cardClass: 'hero-card',
     image: heroImg,
-    title: "heroproject",
-    language: "React, Redux, Firebase",
-    icons: ["fab fa-react", "fas fa-database"],
-    completed: "see page",
-    live_page: "https://hero-project-klauza.firebaseapp.com/signin",
-    github_page: "https://github.com/klauza/heroproject",
-    description: "Login system; Firebase. 2d hero creator. Possible to see others people work too"
+    title: 'Medieval photography',
+    language: 'The History is important',
+    icons: ['fas fa-camera-retro'],
+    completed: 'Link 1',
+    live_page: 'https://hero-project-klauza.firebaseapp.com/signin',
+    github_page: 'https://github.com/klauza/heroproject',
+    description:
+      'Phasellus et aliquet erat. Fusce ornare elit quis erat interdum, et pellentesque quam congue. Duis laoreet, libero id hendrerit lobortis, nunc quam commodo felis, a aliquam sem diam at metus.',
   },
   {
     id: 9,
-    cardClass: "photography-card",
+    cardClass: 'photography-card',
     image: photographyImg,
-    title: "photography",
-    language: "HTML, Sass, jQuery",
-    icons: ["fab fa-html5", "fab fa-sass", "fab fa-js-square"],
-    completed: "see page",
-    live_page: "https://photography-klauza.netlify.com/",
-    github_page: "https://github.com/klauza/photography",
-    description: "Hobbystic photography SPA with some jQuery DOM manipulation"
+    title: 'Forest Guide',
+    language: 'I am being officially a forest guide',
+    icons: ['fas fa-trophy'],
+    completed: 'Link 1',
+    live_page: 'https://photography-klauza.netlify.com/',
+    github_page: 'https://github.com/klauza/photography',
+    description:
+      'Phasellus et aliquet erat. Fusce ornare elit quis erat interdum, et pellentesque quam congue. Duis laoreet, libero id hendrerit lobortis, nunc quam commodo felis, a aliquam sem diam at metus.',
   },
   {
     id: 10,
-    cardClass: "shelter-card",
+    cardClass: 'shelter-card',
     image: petShelterImg,
-    title: "pet shelter",
-    language: "HTML, Sass",
-    icons: ["fab fa-html5", "fab fa-sass"],
-    completed: "see page",
-    live_page: "https://shelter-klauza.netlify.com/",
-    github_page: "https://github.com/klauza/Shelter",
-    description: "Sass based SPA for people who are keen to be taking care of animals. Made with CSS GRID tool"
+    title: 'pet shelter',
+    language: 'I become an owner of a pet shelter',
+    icons: ['fas fa-trophy'],
+    completed: 'Link 1',
+    live_page: 'https://shelter-klauza.netlify.com/',
+    github_page: 'https://github.com/klauza/Shelter',
+    description:
+      'Phasellus et aliquet erat. Fusce ornare elit quis erat interdum, et pellentesque quam congue. Duis laoreet, libero id hendrerit lobortis, nunc quam commodo felis, a aliquam sem diam at metus.',
   },
   {
     id: 11,
-    cardClass: "hussars-card",
+    cardClass: 'hussars-card',
     image: hussarImg,
-    title: "hussars",
-    language: "HTML, Sass, jQuery",
-    icons: ["fab fa-html5", "fab fa-sass"],
-    completed: "see page",
-    live_page: "https://hussars-klauza.netlify.com/",
-    github_page: "https://github.com/klauza/hussars",
-    description: "Horse cavalry at the turn of XVI century. One page website with colorful sections"
-  }
-]
-export {portfData}
+    title: 'Horse cavalry statue',
+    language: 'Quite a view',
+    icons: ['fas fa-camera-retro'],
+    completed: 'Link 1',
+    live_page: 'https://hussars-klauza.netlify.com/',
+    github_page: 'https://github.com/klauza/hussars',
+    description:
+      'Phasellus et aliquet erat. Fusce ornare elit quis erat interdum, et pellentesque quam congue. Duis laoreet, libero id hendrerit lobortis, nunc quam commodo felis, a aliquam sem diam at metus.',
+  },
+];
+export { portfData };
 
 /*
 const portfDataTwo = [
